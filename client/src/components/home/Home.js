2610632288777e7559cec434777181dfd4e9ce2d
rainbowemoji <heroskyweb@gmail.com>
2021-09-04 21:43:59 -0700

    Updated home page

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { useWeb3React } from "@web3-react/core"
 import { injected } from "../wallet/connectors"
 
-function Home() {
+export default function Home() {
   const { active, account, library, connector, activate, deactivate } = useWeb3React()
 
   async function connect() {
@@ -13,22 +13,19 @@ function Home() {
     }
   }
 
-  // async function disconnect() {
-  //   try {
-  //     deactivate()
-  //   } catch (ex) {
-  //     console.log(ex)
-  //   }
-  // }
-
+  async function disconnect() {
+    try {
+      deactivate()
+    } catch (ex) {
+      console.log(ex)
+    }
+  }
 
   return (
-    <div className="home">
-      <h1>Home page</h1>
-
-      <button onClick={connect}>Connect Wallet</button>
+    <div className="flex flex-col items-center justify-center">
+      <button onClick={connect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Connect to MetaMask</button>
+      {active ? <span>Connected with <b>{account}</b></span> : <span>Not connected</span>}
+      <button onClick={disconnect} className="py-2 mt-20 mb-4 text-lg font-bold text-white rounded-lg w-56 bg-blue-600 hover:bg-blue-800">Disconnect</button>
     </div>
-  );
+  )
 }
-
-export default Home;
