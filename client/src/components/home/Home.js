61292b4ba30488fd085ffb68d5447d715089c111
rainbowemoji <heroskyweb@gmail.com>
2021-09-04 22:12:28 -0700

    add react bootstrap

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,6 +1,15 @@
 import React from 'react';
 import { useWeb3React } from "@web3-react/core"
 import { injected } from "../wallet/connectors"
+import { Container } from "react-bootstrap";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
 
 function Home() {
   const { active, account, library, connector, activate, deactivate } = useWeb3React()
@@ -24,9 +33,67 @@ function Home() {
 
   return (
     <div className="home">
-      <h1>Home page</h1>
+      <Container
+        className='MyContainer'
+        style={{
+          justifyContent: "center",
+          alignContent: "center",
+          alignItems: "center",
+          alignSelf: "center",
+        }}
+      >
+        <div
+          style={{
+            textAlign: "center",
+            marginTop: 70,
+            fontSize:40,
+            letterSpacing: "7px",
+            fontFamily: "MyWebFont",
+          }}
+        >
+          I M B U E
+        </div>
+        <div
+          style={{
+            fontFamily: "LuloCleanW01-One",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: 12,
+            lineHeight: "17px",
+            alignItems: "center",
+            textAlign: "center",
+            marginTop: 40,
+            letterSpacing: "5px",
+          }}
+        >
+          LIVESTREAM TO YOUR FAVORITE
+          <br />
+          AUDIENCES AND GET PAID IN CRYPTO
+        </div>
+        <div
+          style={{
+            fontFamily: "LuloCleanW01-One",
+            fontStyle: "normal",
+            fontWeight: "normal",
+            fontSize: "22px",
+            lineHeight: "31px",
+            alignItems: "center",
+            textAlign: "center",
+            marginTop: "20px"
 
-      <button onClick={connect}>Connect Wallet</button>
+          }}
+        >
+          CONNECT YOUR
+          <br /> WALLET TO SIGN IN
+        </div>
+        <div style={{
+          textAlign: "center",
+          marginTop: 150
+        }}>
+          <Link className="wallet-button" to="/connectors">Connect Wallet</Link>
+        </div>
+      </Container>
+
     </div>
   );
 }
